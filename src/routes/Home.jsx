ebdbafbbe3381fb68ef5bdd02b9ca17a623a4838
2003lonewolf <iron57441@gmail.com>
2024-02-05 21:42:27 +0530

    add:img,text

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import Nav from '../assets/components/navbar/Nav'
 import Hero from '../assets/components/hero/Hero'
-import Footer from '../assets/components/footer/Footer'
 const Home = () => {
   return (
     <div>

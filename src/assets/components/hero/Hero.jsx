372cf7f91974f7ee25fd9a7aef31778962a63445
2003lonewolf <iron57441@gmail.com>
2024-02-10 12:05:41 +0530

    add:animation hero

--- src/assets/components/hero/Hero.jsx
+++ src/assets/components/hero/Hero.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import {useEffect} from "react";
+import gsap from "gsap";
+
 import "./Hero.css";
 import amarbg from "../../img/amar-bg.png";
 
 const Hero = () => {
+  useEffect(() => {
+    gsap.from(".character", { duration: 1, y: '100%', ease: 'expo.out',scale:0.6 });
+
+    gsap.to(".spread1", { duration: 1, y: -15, ease: 'power2.out' }); 
+    gsap.to(".spread2", { duration: 1, y: 15, ease: 'power2.out' });
+  }, []);
   return (
     <div className="hero">
       <div className="container">
         <div className="text-container">
-          <h1>amar</h1>
-          <h1>amar</h1>
-          <h1>amar</h1>
+          {/* <h1>amar </h1>  */}
+          <h1 className="spread1">amar </h1> 
+          <h1 className="spread2">amar </h1> 
+          
         </div>
         <div className="character">
           <img src={amarbg} alt="Amar Murmu"  />

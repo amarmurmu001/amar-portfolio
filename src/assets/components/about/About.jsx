eee8decbd5ea61acec2da1a9d2deb4780ebc37b4
amarmurmu001 <iron57441@gmail.com>
2025-12-13 17:22:17 +0530

    feat: add About component with personal bio and image

--- src/assets/components/about/About.jsx
+++ src/assets/components/about/About.jsx
@@ -1,29 +1,29 @@
-import './About.css'
+import "./About.css";
 import amarbg from "../../../assets/img/ChatGPT Image Jul 9, 2025, 04_24_01 PM.png";
 
 const About = () => {
   return (
     <div className="about">
-      <div className='headline'>About me</div>
-      <div className='slogan'>Exploring the depths of Creativity</div>
-      <div className='bio'>
-        <div className='profile-img'>
-          <img 
-            src={amarbg} 
-            alt="Amar Murmu" 
+      <div className="headline">About me</div>
+      <div className="slogan">Exploring the depths of Creativity</div>
+      <div className="bio">
+        <div className="profile-img">
+          <img
+            src={amarbg}
+            alt="Amar Murmu"
             loading="lazy"
             width="300"
             height="300"
           />
         </div>
-        <div className='bio-text'>
-          <div className='bio-headline'>Hellooo! I&apos;m Amar Murmu..</div>
-          <div className='bio-normal'>
-            A web designer from India with 3 years of experience.
-            Specializing in Figma, I create visually stunning and user-friendly websites. 
-            My design philosophy focuses on seamless user experiences. 
-            Check out my portfolio for a glimpse of my work, 
-            and let&apos;s bring your digital visions to life!
+        <div className="bio-text">
+          <div className="bio-headline">Hellooo! I&apos;m Amar Murmu..</div>
+          <div className="bio-normal">
+            A Full Stack Developer from India with a passion for building
+            innovative web applications. I specialize in the JavaScript, React,
+            Next.js, and integrating AI and Blockchain technologies. I enjoy
+            creating seamless user experiences and bringing complex digital
+            visions to life. Check out my portfolio to see my latest work!
           </div>
         </div>
       </div>
